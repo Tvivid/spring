d80bdda538c85888adfd489f9b1bbbf5e66abd29
Sunmyeong Jin <tjsaud3250@naver.com>
2023-06-27 22:49:37 +0900

    AppConfig add

--- core/src/main/java/hellong/core/AppConfig.java
+++ core/src/main/java/hellong/core/AppConfig.java
@@ -1,13 +1,22 @@
 package hellong.core;
 
+import hellong.core.discount.FixDiscountPolicy;
 import hellong.core.member.MemberService;
 import hellong.core.member.MemberServiceImpl;
+import hellong.core.member.MemoryMemberRepository;
+import hellong.core.order.OrderService;
+import hellong.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
     public MemberService memberService(){
-        return new MemberServiceImpl();
+        return new MemberServiceImpl(new MemoryMemberRepository());
     }
+
+    public OrderService orderService(){
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+
 
 
 }
