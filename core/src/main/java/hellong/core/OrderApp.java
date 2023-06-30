ce18a76fa0ed42de9ad209a81335af88a866d3b1
Sunmyeong Jin <tjsaud3250@naver.com>
2023-06-30 20:11:24 +0900

    spring change

--- core/src/main/java/hellong/core/OrderApp.java
+++ core/src/main/java/hellong/core/OrderApp.java
@@ -7,15 +7,20 @@ import hellong.core.member.MemberServiceImpl;
 import hellong.core.order.Order;
 import hellong.core.order.OrderService;
 import hellong.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
 
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId= 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
