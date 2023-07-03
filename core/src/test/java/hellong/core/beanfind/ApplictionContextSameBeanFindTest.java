78a856e5b81f93cfc16448a62d6dc1a2fedf5fa8
Sunmyeong Jin <tjsaud3250@naver.com>
2023-07-03 23:31:57 +0900

    Bean

---
+++ core/src/test/java/hellong/core/beanfind/ApplictionContextSameBeanFindTest.java
@@ -0,0 +1,26 @@
+package hellong.core.beanfind;
+
+import hellong.core.AppConfig;
+import hellong.core.discount.DiscountPolicy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+
+public class ApplictionContextSameBeanFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 중복 오류가 발생한다")
+    void findBeanByTypeDuplicate(){
+        ac.getBean(DiscountPolicy.class);
+    }
+
+    @Configuration
+    static class SameBeanConfig{
+        
+    }
+
+
+}
