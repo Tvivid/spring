5bc9eec621447bdf9c8a91647777a0ab316e9559
Sunmyeong Jin <tjsaud3250@naver.com>
2023-08-02 23:37:24 +0900

    singleton container test

--- core/src/test/java/hellong/core/Singleton/SingletonTest.java
+++ core/src/test/java/hellong/core/Singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import hellong.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -42,6 +44,24 @@ public class SingletonTest {
         //same ==
         //equal
 
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너어와 싱글톤")
+    void springContainer(){
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+
+        //참조값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        //memberService 1 =/= memberService2
+        assertThat(memberService1).isSameAs(memberService2);
     }
 
 
