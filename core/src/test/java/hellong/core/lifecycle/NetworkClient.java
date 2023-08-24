9e9b289cc3522395d45ba22322753b65c2bc62c8
Sunmyeong Jin <tjsaud3250@naver.com>
2023-08-24 16:56:31 +0900

    PostConstruct, PreDestroy

--- core/src/test/java/hellong/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hellong/core/lifecycle/NetworkClient.java
@@ -1,7 +1,10 @@
 package hellong.core.lifecycle;
 
 
-public class NetworkClient  {
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient {
 
     private String url;
 
@@ -28,16 +31,18 @@ public class NetworkClient  {
         System.out.println("close : " + url);
     }
 
-
+    @PostConstruct
     public void init() {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-
+    @PreDestroy
     public void close() {
-        System.out.println("NetworkClient.destroy");
+        System.out.println("NetworkClient.close");
         disconnect();
     }
+
+
 }
