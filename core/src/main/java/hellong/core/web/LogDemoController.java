2e9dbbb1859067ad1e76d72ef6adf283bf72ca42
Sunmyeong Jin <tjsaud3250@naver.com>
2023-08-31 22:58:36 +0900

    Scope-provider

--- core/src/main/java/hellong/core/web/LogDemoController.java
+++ core/src/main/java/hellong/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hellong.core.web;
 
 import hellong.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,14 +15,14 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-
-    
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request){
+
         String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
